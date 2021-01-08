0146d963286d645fa8f215e82bbee8574e8a1ea1
nijinnath <nijinnath@gmail.com>
2021-01-08 23:01:09 +0530

    Update server.js
    Display text changed.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World. First Jenkin container docker image created for node js application.');
 });
 
 app.listen(PORT, HOST);
